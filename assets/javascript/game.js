b9de63e6d3b1378b4dc16c622db89200d6706887
Face70 <60823541+DarkKnight70@users.noreply.github.com>
2020-03-09 22:27:22 -0600

    updated style sheet and javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,3 +5,49 @@ if(!isChrome){
     $('#playAudio').remove();
 } //trying to get background audio to play in chrome, still no success
 
+var words = [
+    "clint eastwood",
+    "john wayne",
+    "horse",
+    "cowboy",
+    "lasso",
+    "saddle",
+    "six shooter",
+    "tombstone",
+    "wyatt earp",
+    "sheriff",
+    "cattle",
+    "gunslinger",
+    "ranch",
+    "rodeo"
+];
+
+const attempts = 10
+
+var guessedLetters = [];
+var currentWord;
+var guessingWord = [];
+var remainingGuesses = 0;
+var started = false;
+var finished = false;
+var wins = 0;
+
+function reset() {
+    remaingingGuesses = attempts;
+    started = false;
+
+    currentWord = Math.floor(Math.random() * (selectableWords.length));
+
+    guessedLetters = [];
+    guessingWord = [];
+
+    for (var i = 0; i < words[currentWord].length; i++) {
+        guessingWord.push("_");
+    }
+
+    document.getElementById("tryAgain").style.cssText = "display: none";
+
+    display();
+
+};
+
